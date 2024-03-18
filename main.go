0a11b45b29d0c386c3ce9185afce584d9db8763f
yobicod <yobicod.4u@gmail.com>
2024-03-18 13:35:34 +0700

    feat: imp go api find all and find by id

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type book struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price"`
+}
+
+var books = []book{
+	{
+		ID:     "1",
+		Name:   "Harry Potter",
+		Author: "J.K. Rowling",
+		Price:  15.9,
+	},
+	{
+		ID:     "2",
+		Name:   "One Piece",
+		Author: "Oda Eiichirō",
+		Price:  2.99,
+	},
+	{
+		ID:     "3",
+		Name:   "demon slayer",
+		Author: "koyoharu gotouge",
+		Price:  2.99,
+	},
+}
+
+func getAllBooks(c *gin.Context) {
+	c.JSON(http.StatusOK, books)
+}
+
+func getBookById(c *gin.Context) {
+	paramID := c.Param("id")
+	for _, book := range books {
+		if book.ID == paramID {
+			c.JSON(http.StatusOK, book)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, "Data not found")
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getAllBooks)
+	router.GET("/books/:id", getBookById)
+	router.Run("localhost:8080")
+}
